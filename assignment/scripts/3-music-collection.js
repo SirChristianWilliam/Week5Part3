bbe21efedf6771a6eb7b94cba845ac12ffb7e63e
Chris Maki <c.william.developer@gmail.com>
2022-09-12 16:21:02 -0500

    Created function that adds the input values to the collections array

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,2 +1,11 @@
 console.log('***** Music Collection *****')
-console.log("testing link");
+let collection = []; 
+
+ 
+function addToCollection(title,artist,yearPublished) {
+collection = [{title,artist,yearPublished}];
+ return collection[0];
+}
+
+console.log(addToCollection("Cool","Danny Devito",2004));
+console.log(collection[0]) ;
