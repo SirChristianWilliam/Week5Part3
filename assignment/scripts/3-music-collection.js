dff996e4fb566646e9b4c447c30b5317cb6e02d6
Chris Maki <c.william.developer@gmail.com>
2022-09-12 17:15:37 -0500

    Found errors in function when testing, fixed them

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,11 +1,18 @@
 console.log('***** Music Collection *****')
 let collection = []; 
-
  
+  
 function addToCollection(title,artist,yearPublished) {
-collection = [{title,artist,yearPublished}];
- return collection[0];
+    let obj = {title,artist,yearPublished};
+  collection.push(obj);
+   return obj; 
 }
 
-console.log(addToCollection("Cool","Danny Devito",2004));
-console.log(collection[0]) ;
+console.log(addToCollection("Egg","Danny Devito",2004)); 
+console.log(addToCollection("Thriller","Michael Jackson",1982));
+console.log(addToCollection("Abbey Road","The Beatles",1969));
+console.log(addToCollection("Bad","Michael Jackson",1987));
+console.log(addToCollection("Let It Be","The Beatles",1970));
+console.log(addToCollection("Best Day Ever","Mac Miller",2011));
+console.log(collection);
+ 
