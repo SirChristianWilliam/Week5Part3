e3c32edf86f21e2814779f76e0a7fb958d0a2362
Chris Maki <c.william.developer@gmail.com>
2022-09-13 15:22:00 -0500

    Cleaned up empty spaces

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,13 +1,12 @@
 console.log('***** Music Collection *****')
 let collection = []; 
 console.log(collection);
- 
   
 function addToCollection(title,artist,yearPublished) {
     let obj = {title,artist,yearPublished};
   collection.push(obj);
    return obj; 
-}
+};
 
 console.log(addToCollection("Egg","Danny Devito",2004)); 
 console.log(addToCollection("Thriller","Michael Jackson",1982));
@@ -16,10 +15,6 @@ console.log(addToCollection("Bad","Michael Jackson",1987));
 console.log(addToCollection("Let It Be","The Beatles",1970));
 console.log(addToCollection("Best Day Ever","Mac Miller",2011));
 console.log(collection);
-
- 
-
-  
 
 function showCollection(arr) {
     let text;
@@ -30,8 +25,7 @@ function showCollection(arr) {
          for(let j of arr) {
             text = j;
          }
-    }
- 
+    };
 };
  showCollection(collection);
 
@@ -58,6 +52,7 @@ function showCollection(arr) {
  showCollection(rap);
 // It checks out!
 console.log("-----break-----")
+
    function findByArtist(artist) {
     let matching = [];
        for(let x of collection) {
@@ -65,14 +60,26 @@ console.log("-----break-----")
             console.log(x);
             matching.push(x);
            }  
-   } 
-   return matching;
+   };
+   return  matching;
  };
 
 console.log(findByArtist("The Beatles"));
 console.log(findByArtist("The Wiggles"));
 console.log(findByArtist("NF"));
+console.log(findByArtist("Danny Devito"));
 
+
+// So, I'm going to ask you about this question in class this week if we have time.
+// If not, I'll ask here. So, the function works exactly as it should, for all
+// intents and purposes. However, when I add an "else" onto my if statement, it does
+// not return as I would intend. Because in my collections array I have one more object
+// at the end, the for loop reads that as the last value and no matter what will
+// return and empty array []. For example, what if I would like to return the contents
+// of the if statement (matching.push(x)), and if that statement is not true for all
+// of the array's contents, then I would like to return a string "false". 
+// I could not find a way to accomplish this and I've spent probably around 5 hours
+// or more on it now. I have a working function, so I will be moving on for now. Thanks.
 
 
 
